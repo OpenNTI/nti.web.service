5b4e0b1f6d11402f1c203cbcd7a8ed21f901231c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-01 15:20:02 -0500

    do not load the server boostrap in tests

--- test/index.js
+++ test/index.js
@@ -5,4 +5,9 @@ const glob = require( 'glob' );
 
 
 //This is for coverage
-glob.sync( `${__dirname}/../src/**/*.js` ).forEach(file => require( file ) );
+glob.sync( `${__dirname}/../src/**/*.js` )
+	.forEach(file => {
+		if (!/src\/index.js/.test(file)) {
+			require( file );
+		}
+	});

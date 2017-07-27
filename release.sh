50015410ed93d7532ae378df1d1910934a82dd4d
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-07-27 12:19:50 -0500

    add a release script

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+#sometimes git returns strange things...this seems to clear the bad state.
+git status &> /dev/null
+
+#check for unstaged tracked files
+if ! git diff-files --quiet; then
+	echo 'There are uncommitted changes. Aborting.'
+	exit 1
+fi
+
+#check for staged/not committed files
+if ! git diff-index --quiet --cached HEAD; then
+	echo 'There are uncommitted changes. Aborting.'
+	exit 1
+fi
+
+mocha || (echo "There are test failures!" && exit 1)
+
+NEW=`npm version minor`
+NEXT=`semver -i minor $NEW`
+NEXT=$NEXT-alpha
+npm version $NEXT > /dev/null
+git tag -d v$NEXT > /dev/null

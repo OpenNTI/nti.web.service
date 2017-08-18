d6ae07b47a5f4f253f24a568ebb9ee1a107665d3
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-08-18 16:19:50 -0500

    add regenerator-runtime for transpiled async functions

--- src/polyfills.js
+++ src/polyfills.js
@@ -2,5 +2,6 @@
 (() => {//iife for istanbul ignore next
 	//add polyfills and shims
 	require('core-js');
+	require('regenerator-runtime/runtime');
 	global.fetch = global.fetch || require('node-fetch');
 })();

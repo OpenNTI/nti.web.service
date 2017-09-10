6af16873c49de6ce1ccaab4162c126c25e6dbb3c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-09-10 14:44:58 -0500

    cleanup config

--- src/__test__/__jest.config.ci.js
+++ src/__test__/__jest.config.ci.js
@@ -1,16 +1,18 @@
+const path = require('path');
+
 const {jest} = require('../../package.json') || {};
 
+process.env.JEST_JUNIT_OUTPUT = 'reports/test-results/index.xml';
+
 module.exports = Object.assign(jest || {}, {
-	collectCoverage: true,
-	collectCoverageFrom: ['src/**/*.{js,jsx}','!**/*.spec.js'],
+	collectCoverage: Boolean(process.env.CI),
+	collectCoverageFrom: ['src/**/*.{js}','!**/*.spec.js', '!**/__test__/**.js'],
 	coverageDirectory: 'reports/coverage',
 	coverageReporters: process.env.CI ? [
 		'text-summary',
 		'lcov',
 		'cobertura'
 	] : ['text-summary'],
-	testPathIgnorePatterns: [
-		'<rootDir>[/\\\\](build|docs|node_modules|scripts)[/\\\\]',
-	],
+	rootDir: path.resolve(__dirname, '../..'),
 	testResultsProcessor: process.env.CI ? './node_modules/jest-junit' : void 0,
 });

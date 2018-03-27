77c86cefc695598f7608d400352c05be650ef907
Jonathan Grimes <jsg2021@gmail.com>
2018-03-27 15:57:17 -0500

    add browser impls of base64 encode/decode

--- src/polyfills.js
+++ src/polyfills.js
@@ -3,5 +3,7 @@
 	//add polyfills and shims
 	// require('core-js');
 	require('regenerator-runtime/runtime');
+	global.atob = require('atob');
+	global.btoa = require('btoa');
 	global.fetch = global.fetch || require('node-fetch');
 })();

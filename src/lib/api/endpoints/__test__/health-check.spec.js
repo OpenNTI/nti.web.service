0643f1b346ff3c0c8f1842c761cf7d168349e6ee
Jonathan Grimes <jsg2021@gmail.com>
2018-09-14 16:31:58 -0500

    update _ops/ping to always instantly respond with 200. add _ops/ping-through to perform the ops/ping to the ds too.

--- src/lib/api/endpoints/__test__/health-check.spec.js
+++ src/lib/api/endpoints/__test__/health-check.spec.js
@@ -1,6 +1,11 @@
 /*eslint-env jest*/
 'use strict';
 
+const PING_THROUGH = /^\/_ops\/ping-through/;
+const PING = /^\/_ops\/ping/;
+
+const REQ = (a, b) => a.source === b.source;
+const getCallback = (calls, RE) => (calls.find(x => REQ(x[0], RE)) || [])[1];
 
 describe ('lib/api/endpoints/heath-check', () => {
 
@@ -14,25 +19,24 @@ describe ('lib/api/endpoints/heath-check', () => {
 	});
 
 
-	test ('registers _ops/ping', () => {
+	test ('registers _ops/ping(s)', () => {
 		const {default: register} = require('../health-check');
 		const api = {get: jest.fn()};
 
 		expect(() => register(api, {}, {})).not.toThrow();
-		expect(api.get).toHaveBeenCalledTimes(1);
 		expect(api.get).toHaveBeenCalledWith(expect.any(RegExp), expect.any(Function));
 	});
 
 
-	test ('_ops/ping calls server.get(_ops/ping)', () => {
+	test ('_ops/ping-through calls server.get(_ops/ping)', () => {
 		const {default: register} = require('../health-check');
 		const api = {get: jest.fn()};
 		const server = {get: jest.fn(() => Promise.resolve())};
 
 		expect(() => register(api, {}, server)).not.toThrow();
-		expect(api.get).toHaveBeenCalledTimes(1);
 		expect(api.get).toHaveBeenCalledWith(expect.any(RegExp), expect.any(Function));
-		const [, callback] = api.get.mock.calls[0];
+
+		const callback = getCallback(api.get.mock.calls, PING_THROUGH);
 		const res = {
 			status: jest.fn()
 		};
@@ -48,15 +52,14 @@ describe ('lib/api/endpoints/heath-check', () => {
 	});
 
 
-	test ('_ops/ping 503\'s if anything goes wrong.', () => {
+	test ('_ops/ping-through 503\'s if anything goes wrong.', () => {
 		const {default: register} = require('../health-check');
 		const api = {get: jest.fn()};
 		const server = {get: jest.fn(() => Promise.reject())};
 
 		expect(() => register(api, {}, server)).not.toThrow();
-		expect(api.get).toHaveBeenCalledTimes(1);
 		expect(api.get).toHaveBeenCalledWith(expect.any(RegExp), expect.any(Function));
-		const [, callback] = api.get.mock.calls[0];
+		const callback = getCallback(api.get.mock.calls, PING_THROUGH);
 		const res = {
 			status: jest.fn()
 		};
@@ -68,6 +71,30 @@ describe ('lib/api/endpoints/heath-check', () => {
 			.then(() => {
 				expect(res.status).toHaveBeenCalledTimes(1);
 				expect(res.status).toHaveBeenCalledWith(503);
+			});
+	});
+
+
+	test ('_ops/ping responds with 200.', () => {
+		const {default: register} = require('../health-check');
+		const api = {get: jest.fn()};
+		const server = {get: jest.fn(() => Promise.reject())};
+
+		expect(() => register(api, {}, server)).not.toThrow();
+		expect(api.get).toHaveBeenCalledTimes(2);
+		expect(api.get).toHaveBeenCalledWith(expect.any(RegExp), expect.any(Function));
+		const callback = getCallback(api.get.mock.calls, PING);
+		const res = {
+			status: jest.fn()
+		};
+
+		return new Promise(finish => {
+			res.end = finish;
+			callback({}, res);
+		})
+			.then(() => {
+				expect(res.status).toHaveBeenCalledTimes(1);
+				expect(res.status).toHaveBeenCalledWith(200);
 			});
 	});
 });
--- src/lib/api/endpoints/health-check.js
+++ src/lib/api/endpoints/health-check.js
@@ -2,12 +2,16 @@
 
 exports.default = function register (api, config, server) {
 
-	api.get(/^\/_ops\/ping/, (SERVER_CONTEXT, res) => {
-
+	api.get(/^\/_ops\/ping-through/, (SERVER_CONTEXT, res) => {
 		server.get('/_ops/ping', SERVER_CONTEXT)
 			.then(() => 200, () => 503)
 			.then(status => {
 				res.status(status); res.end();
 			});
+	});
+
+	api.get(/^\/_ops\/ping/, (_, res) => {
+		res.status(200);
+		res.end();
 	});
 };

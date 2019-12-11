0ccd59f32a175cfbb4421857e981454a1af2225b
Jonathan Grimes <jsg2021@gmail.com>
2019-12-11 11:02:19 -0600

    polyfill AbortController

--- src/polyfills.js
+++ src/polyfills.js
@@ -6,5 +6,6 @@
 	require('regenerator-runtime/runtime');
 	global.atob = require('atob');
 	global.btoa = require('btoa');
+	global.AbortController = global.AbortController || require('abort-controller');
 	global.fetch = global.fetch || require('node-fetch');
 })();

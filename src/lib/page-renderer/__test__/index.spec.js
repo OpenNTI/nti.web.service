1e29107c62aca29d49a6c0977a7f0a165baeee3f
Jonathan Grimes <jsg2021@gmail.com>
2020-04-21 18:15:53 -0500

    NTI-9171 - Make value escaping explicit

--- src/lib/page-renderer/__test__/index.spec.js
+++ src/lib/page-renderer/__test__/index.spec.js
@@ -2,6 +2,8 @@
 'use strict';
 jest.mock('fs');
 
+const unTab = x => (x[0].trim().replace(/\t+/g, ''));
+
 describe('lib/page-renderer (index)', () => {
 
 	beforeEach(() => {
@@ -38,7 +40,7 @@ describe('lib/page-renderer (index)', () => {
 		const fn = require('../index').getRenderer;
 
 		jest.spyOn(fs, 'stat').mockImplementation(async (f) => ({ mtime: new Date('2018-04-02T16:35:42.000Z') }));
-		jest.spyOn(fs, 'readFile').mockImplementation(async (f, _, cb) => '');
+		jest.spyOn(fs, 'readFile').mockImplementation(async (f) => '');
 
 		expect(fn).toEqual(expect.any(Function));
 		expect(fn.length).toEqual(3);
@@ -48,6 +50,50 @@ describe('lib/page-renderer (index)', () => {
 		const result = await render('/');
 		expect(result).toEqual('Bad Template');
 	});
+
+	describe('Variables', () => {
+		const fs = require('fs').promises;
+		const fn = require('../index').getRenderer;
+
+		jest.spyOn(fs, 'stat').mockImplementation(async (f) => ({ mtime: new Date('2018-04-02T16:35:42.000Z') }));
+
+		beforeEach(() => jest.clearAllMocks());
+
+		test('CDATA style', async () => {
+			jest.spyOn(fs, 'readFile').mockImplementation(async (f) => `
+				<![CDATA[cfg:title]]>
+				<![CDATA[cfg:title|html]]>
+				<![CDATA[cfg:title|string]]>
+			`);
+
+
+			const render = fn('/test/');
+			const result = await render('/', {}, { config: { title: 'Kibbles \'n Bits' } });
+			expect(result).toEqual(`
+				Kibbles &apos;n Bits
+				Kibbles &apos;n Bits
+				Kibbles \\'n Bits
+			`);
+		});
+
+		test('inline style', async () => {
+			jest.spyOn(fs, 'readFile').mockImplementation(async (f) => `
+				<[cfg:title]>
+				<[cfg:title|html]>
+				<[cfg:title|string]>
+			`);
+
+
+			const render = fn('/test/');
+			const result = await render('/', {}, { config: { title: 'Kibbles \'n Bits' } });
+			expect(result).toEqual(`
+				Kibbles &apos;n Bits
+				Kibbles &apos;n Bits
+				Kibbles \\'n Bits
+			`);
+		});
+	});
+
 
 	// test ('render (root path rewrite)');
 	// test ('render (config value injection)');
--- src/lib/page-renderer/index.js
+++ src/lib/page-renderer/index.js
@@ -1,7 +1,7 @@
 /*eslint strict: 0*/
 'use strict';
 
-const {URL: {join: urlJoin}} = require('@nti/lib-commons');
+const { URL: { join: urlJoin } } = require('@nti/lib-commons');
 
 const {
 	getTemplate,
@@ -11,30 +11,57 @@ Object.assign(exports, {
 	getRenderer
 });
 
-const NOOP = () => {};
+const Identity = x => x;
+const NOOP = () => { };
+const ESCAPES = {
+	html: x => x
+		.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;')
+		.replace(/"/g, '&quot;')
+		.replace(/'/g, '&apos;'),
+
+	string: x => x
+		.replace(/\\/g, '\\\\')
+		.replace(/\n/g, '\\n')
+		.replace(/\r/g, '\\r')
+		.replace(/'/g, '\\\'')
+		.replace(/"/g, '\\"')
+		.replace(/`/g, '\\`'),
+};
 
 const isRootPath = RegExp.prototype.test.bind(/^\/(?!\/).*/);
 const isSiteAssets = RegExp.prototype.test.bind(/^\/site-assets/);
-const isVendoredAssets = RegExp.prototype.test.bind(/^\/vendor/);
+const isVendorAssets = RegExp.prototype.test.bind(/^\/vendor/);
 const isFavicon = RegExp.prototype.test.bind(/^\/favicon\.ico/);
 
 const shouldPrefix = (val, base) => isRootPath(val)
-							&& !val.startsWith(base)
-							&& !isSiteAssets(val)
-							&& !isVendoredAssets(val)
-							&& !isFavicon(val);
+	&& !val.startsWith(base)
+	&& !isSiteAssets(val)
+	&& !isVendorAssets(val)
+	&& !isFavicon(val);
 
 const attributesToFix = /(manifest|src|href)="(.*?)"/igm;
 const fixAttributes = (base, original, attr, val) => `${attr}="${shouldPrefix(val, base) ? urlJoin(base, val) : val}"`;
 
 const configValues = /<(!\[CDATA)?\[cfg:([^\]]*)\]\]?>/igm;
-const fillInValues = (cfg, orginal, _, prop) => cfg[prop] === null ? '' : (cfg[prop] || `MissingConfigValue[${prop}]`);
+const fillInValues = (cfg, original, _, prop) => {
+	const [
+		propertyName,
+		filter = 'html'
+	] = prop.split('|');
+	const value = cfg[propertyName] === null ? '' : (cfg[propertyName] || `MissingConfigValue[${propertyName}]`);
+
+	const filterFn = ESCAPES[filter] || Identity;
+
+	return filterFn(value);
+};
 
 
 function getRenderer (assets, renderContent, devmode) {
 
 	if (!renderContent) {
-		renderContent = ({html}) => html;
+		renderContent = ({ html }) => html;
 	}
 
 	/**
@@ -46,7 +73,7 @@ function getRenderer (assets, renderContent, devmode) {
 	 *                                      that fulfills with a string.
 	 * @return {Promise<string>}            Fulfills with the rendered page as a string
 	 */
-	return async function render (basePath, {url, config: {templateInjections} = {}} = {}, {html, config} = {}, markError = NOOP) {
+	return async function render (basePath, { url, config: { templateInjections } = {} } = {}, { html, config } = {}, markError = NOOP) {
 		const template = (await getTemplate(assets, templateInjections, devmode)) || 'Bad Template';
 
 		// There are non-enumerable properties on config, and we need them passed to renderContent, so create a new cfg

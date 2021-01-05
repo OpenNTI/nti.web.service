f6dcf8239e583668f024bd482439d0a6d13efd3b
Jonathan Grimes <jsg2021@gmail.com>
2021-01-05 11:34:53 -0600

    drop stupid simple polyfill deps implement them in-house

--- src/polyfills.js
+++ src/polyfills.js
@@ -4,8 +4,11 @@
 	//add polyfills and shims
 	// require('core-js');
 	require('regenerator-runtime/runtime');
-	global.atob = require('atob');
-	global.btoa = require('btoa');
 	global.AbortController = global.AbortController || require('abort-controller');
 	global.fetch = global.fetch || require('node-fetch');
+
+	global.atob = x => Buffer.from(x, 'base64').toString('binary');
+	global.btoa = x =>
+		((x instanceof Buffer) ? x : Buffer.from(x.toString(), 'binary')).toString('base64');
+
 })();

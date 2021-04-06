680da260437d1fda638727ac7fd50dde6761f4d5
Jonathan Grimes <jsg2021@gmail.com>
2021-04-06 12:59:05 -0500

    Make the example import an esm file

--- example/index.js
+++ example/index.js
@@ -1,20 +1,17 @@
 'use strict';
-const {join} = require('path');
+const { join } = require('path');
 
 exports = module.exports = {
-
-	register (/*expressApp , config, requestRestart*/) {
-
+	async register(/*expressApp , config, requestRestart*/) {
+		await import('./esm.mjs');
 		return {
 			devmode: null,
 
 			assets: __dirname,
 
-			renderContent ({html, url, basepath}) {
+			renderContent({ html, url, basepath }) {
 				return 'Page! at ' + join(basepath, url) + html;
-			}
+			},
 		};
-
-	}
-
+	},
 };
